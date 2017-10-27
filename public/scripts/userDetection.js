583065a02bdc7b535655a45e3fae23a188c14b87
F11 <ds@develup.pro>
2017-10-27 19:41:34 +0300

    asdasdasdsssewrfasd

--- public/scripts/userDetection.js
+++ public/scripts/userDetection.js
@@ -3,7 +3,7 @@ var link = location.href;
 
 if(link.indexOf('?deviceId=') != -1) {
     console.log(location);
-
+    var ac = new Accelerometer();
 
 } else {
     var linkArea = document.getElementById('connection-link');
